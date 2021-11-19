80e7068724de25afb473e4a476524c5dbca92ea8
FnrDev <ah2014bh@outlook.com>
2021-11-19 20:12:41 +0300

    delete ticket command

---
+++ commands/ticket/delete.js
@@ -0,0 +1,45 @@
+const { MessageActionRow, MessageButton } = require('discord.js');
+
+module.exports = {
+    name: "delete",
+    description: "Delete a ticket channel.",
+    modOnly: true,
+    ticketOnly: true,
+    run: async(interaction) => {
+        const row = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+            .setCustomId('delete_ticket')
+            .setStyle('DANGER')
+            .setLabel('Delete')
+        )
+        .addComponents(
+            new MessageButton()
+            .setCustomId('cancel')
+            .setStyle('SECONDARY')
+            .setLabel('Cancel')
+        )
+        const msg = await interaction.reply({
+            content: `Are you sure to delete **#${interaction.channel.name}** ticket.`,
+            fetchReply: true,
+            components: [row]
+        });
+        const filter = i => i.customId === 'delete_ticket' || 'cancel' && i.user.id === interaction.user.id;
+        const collector = interaction.channel.createMessageComponentCollector({ filter: filter, time: 20000 });
+        collector.on('collect', async i => {
+            if (i.customId === 'delete_ticket') {
+                return interaction.channel.delete();
+            }
+            if (i.customId === 'cancel') {
+                await interaction.deleteReply();
+            }
+        });
+        collector.on('end', async i => {
+            try {
+                await interaction.deleteReply();
+            } catch (e) {
+                return false;
+            }
+        })
+    }
+}

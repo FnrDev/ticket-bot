4b6699aa762a8b2613ff264569ce81935ec60209
FnrDev <ah2014bh@outlook.com>
2021-11-20 17:11:56 +0300

    close ticket

---
+++ commands/ticket/close.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const config = require('../../settings.json')
+
+module.exports = {
+    name: "close",
+    description: "Close a ticket.",
+    ticketOnly: true,
+    modOnly: true,
+    category: "ticket",
+    run: async(interaction) => {
+        await interaction.channel.permissionOverwrites.set([
+            {
+                id: interaction.guild.id,
+                deny: ['VIEW_CHANNEL', 'SEND_MESSAGES']
+            }
+        ])
+        const embed = new Discord.MessageEmbed()
+        .setDescription(`**🔒 Ticket has been closed**`)
+        .setColor(config.embedColor)
+        interaction.reply({
+            embeds: [embed]
+        })
+    }
+}

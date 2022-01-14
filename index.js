2737ca2bf6cf021742f8d1f3ba67b515a279106c
Fnr <ah2014bh@outlook.com>
2022-01-14 11:24:51 +0300

    init database

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
+const mysql = require('mysql-database');
+const database = new mysql();
 client.commands = new Discord.Collection();
 client.slash = new Discord.Collection();
 client.aliases = new Discord.Collection();
 require('dotenv').config();
+require('colors');
 
 ['hanlders', 'events'].forEach(handler => {
   require(`./handlers/${handler}`)(client);
 });
+
+(async () => {
+  const db = await database.connect({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    user: process.env.USER,
+    database: process.env.DATABASE
+  });
+  client.db = db;
+  db.on('connected', () => {
+    console.log(`[DataBase] DataBase Connected.`.green);
+  })
+})();
 
 client.login(process.env.TOKEN);

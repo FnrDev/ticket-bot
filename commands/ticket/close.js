49da6af96171be68d0d9bdccbd1d085e692eaca4
Fnr <ah2014bh@outlook.com>
2022-01-15 09:56:32 +0300

    set false for open data

--- commands/ticket/close.js
+++ commands/ticket/close.js
@@ -8,13 +8,16 @@ module.exports = {
     modOnly: true,
     category: "ticket",
     usage: "/close",
-    run: async(interaction) => {
+    run: async(interaction, client) => {
         await interaction.channel.permissionOverwrites.set([
             {
                 id: interaction.guild.id,
                 deny: ['VIEW_CHANNEL', 'SEND_MESSAGES']
             }
-        ])
+        ]);
+        const ticketData = await client.db.get('tickets', interaction.channel.id);
+        ticketData['open'] = false;
+        await client.db.set('tickets', interaction.channel.id, ticketData);
         const embed = new Discord.MessageEmbed()
         .setDescription(`**🔒 Ticket has been closed**`)
         .setColor(config.embedColor)

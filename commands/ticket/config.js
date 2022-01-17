a48e92322222ff176872382dd2a14380bb62ac82
Fnr <ah2014bh@outlook.com>
2022-01-17 05:24:50 +0300

    add embed content to config

--- commands/ticket/config.js
+++ commands/ticket/config.js
@@ -26,6 +26,11 @@ module.exports = {
             name: "error_color",
             description: "The hex color for error embed color",
             type: 3
+        },
+        {
+            name: "embed_content",
+            description: "The content for the embed",
+            type: 3
         }
     ],
     run: async(interaction, client) => {
@@ -33,16 +38,18 @@ module.exports = {
         const role = interaction.options.getRole('role');
         const successColor = interaction.options.getString('embed_color') || null;
         const errorColor = interaction.options.getString('error_color') || null;
+        const embedcontent = interaction.options.getString('embed_content') || null;
         const resolveEmbedColor = Util.resolveColor(successColor);
         const resolveErrorColor = Util.resolveColor(errorColor);
         await client.db.set('config', interaction.guild.id, {
             category: category.id,
             role: role.id,
             successColor: resolveEmbedColor,
-            errorColor: resolveErrorColor
+            errorColor: resolveErrorColor,
+            content: embedcontent
         });
         interaction.reply({
-            content: `✅ Config has been set!\n**Role :** ${role.name}\n**Category :** ${category}\n**Embed Color :** ${successColor || 'No Color'}\n**Error Embed Color :** ${errorColor || 'No Color'}`
+            content: `✅ Config has been set!\n**Role :** ${role.name}\n**Category :** ${category}\n**Embed Color :** ${successColor || 'No Color'}\n**Error Embed Color :** ${errorColor || 'No Color'}\n**Embed Content :** ${embedcontent}`
         })
     }
 }

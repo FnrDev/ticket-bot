d029e50be9f60bbe4862eda16e298d55619cc34b
Fnr <ah2014bh@outlook.com>
2022-02-02 21:45:33 +0300

    refactor: config

--- commands/ticket/config.js
+++ commands/ticket/config.js
@@ -1,67 +1,56 @@
-const { Util, MessageEmbed } = require('discord.js');
+const { Util } = require('discord.js');
 const configOptions = require('../../configOptions');
 
 module.exports = {
     name: "config",
-    description: "Config ticket system.",
+    description: "Configuration ticket system.",
     options: configOptions,
     run: async(interaction, client) => {
-        const category = interaction.options.getChannel('category');
-        const role = interaction.options.getRole('role');
-        const successColor = interaction.options.getString('embed_color') || null;
-        const errorColor = interaction.options.getString('error_color') || null;
-        const embedcontent = interaction.options.getString('embed_content') || 'Support will be with you shortly.';
-        const limit = interaction.options.getInteger('limit_per_user') || 1;
-        const defaultName = interaction.options.getString('default_name') || null;
-        const logChannel = interaction.options.getChannel('log_channel');
-        const resolveEmbedColor = Util.resolveColor(successColor);
-        const resolveErrorColor = Util.resolveColor(errorColor);
-        await client.db.set('config', interaction.guild.id, {
-            category: category ? category.id : null,
-            role: role ? role.id : null,
-            successColor: resolveEmbedColor,
-            errorColor: resolveErrorColor,
-            content: embedcontent,
-            limit_per_user: limit,
-            name: defaultName,
-            log_channel: logChannel ? logChannel.id : null
-        });
-        const embed = new MessageEmbed()
-        .setAuthor(`${interaction.guild.name} Config`, interaction.guild.iconURL())
-        .setColor(interaction.guild.me.displayHexColor)
-        .addFields(
-            {
-                name: "Role :",
-                value: role ? role.name : 'No role has been setup'
-            },
-            {
-                name: "Category :",
-                value: category ? category.toString() : 'No Category'
-            },
-            {
-                name: "Embed Color :",
-                value: successColor || 'No Color'
-            },
-            {
-                name: "Error Embed Color :",
-                value: errorColor || 'No Color'
-            },
-            {
-                name: "Embed Content :",
-                value: embedcontent || 'No Embed Content'
-            },
-            {
-                name: "Default Ticket Name :",
-                value: defaultName || 'No Default Name'
-            },
-            {
-                name: "Log Channel :",
-                value: logChannel ? logChannel.toString() : "No Log Channel"
-            }
-        )
-        interaction.reply({
-            embeds: [embed],
-            content: "**✅ Config has been set! 🥳**"
-        })
+        if (interaction.options.getSubcommand() === 'message') {
+            const message = interaction.options.getString('message');
+            let data = await client.db.get('config', interaction.guild.id);
+            if (!data) data = {};
+            data.message = message;
+            await client.db.set('config', interaction.guild.id, data);
+            return interaction.reply({
+                content: "Config has been set!"
+            })
+        }
+        if (interaction.options.getSubcommand() === 'category') {
+            const channel = interaction.options.getChannel('category');
+            let data = await client.db.get('config', interaction.guild.id);
+            if (!data) data = {};
+            data.category = channel.id;
+            await client.db.set('config', interaction.guild.id, data);
+            return interaction.reply({
+                content: "Config has been set!"
+            })
+        }
+        if (interaction.options.getSubcommand() === 'color') {
+            const success = interaction.options.getString('success');
+            const error = interaction.options.getString('error');
+            const resovleSuccess = Util.resolveColor(success);
+            const resloveError = Util.resolveColor(error);
+            let data = await client.db.get('config', interaction.guild.id);
+            if (!data) data = {};
+            data.success = resovleSuccess;
+            data.error = resloveError;
+            await client.db.set('config', interaction.guild.id, data);
+            return interaction.reply({
+                content: "Config has been set!"
+            }) 
+        }
+        if (interaction.options.getSubcommand() === 'role') {
+            const staffRole = interaction.options.getRole('staff');
+            const managersRole = interaction.options.getRole('managers');
+            let data = await client.db.get('config', interaction.guild.id);
+            if (!data) data = {};
+            data.staff = staffRole.id;
+            data.managers = managersRole.id;
+            await client.db.set('config', interaction.guild.id, data);
+            return interaction.reply({
+                content: "Config has been set!"
+            })
+        }
     }
 }

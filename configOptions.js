e99c263a83390235af7bec5c1bcc30dba37e3216
Fnr <ah2014bh@outlook.com>
2022-02-02 21:46:56 +0300

    refactor: config options

--- configOptions.js
+++ configOptions.js
@@ -1,46 +1,67 @@
 module.exports = [
     {
-        name: "role",
-        description: "Role to view ticket channel",
-        type: 8,
-        required: true
+        name: "message",
+        description: "Configuration your ticket message",
+        type: 1,
+        options: [
+            {
+                name: "message",
+                description: "The message to sent in ticket",
+                type: 3,
+                required: true
+            }
+        ]
     },
     {
         name: "category",
-        description: "Category to ticket create in",
-        type: 7,
-        channel_types: [4],
-        required: true
+        description: "Configuration your ticket category.",
+        type: 1,
+        options: [
+            {
+                name: "category",
+                description: "Select category to create ticket in",
+                type: 7,
+                channel_types: [4],
+                required: true
+            }
+        ]
     },
     {
-        name: "embed_color",
-        description: "The hex color for embed color",
-        type: 3
+        name: "color",
+        description: "Configuration your embed color",
+        type: 1,
+        options: [
+            {
+                name: "success",
+                description: "The success embed hex color",
+                type: 3,
+                required: true
+            },
+            {
+                name: "error",
+                description: "The error embed hex color",
+                type: 3,
+                required: true
+            }
+        ]
     },
     {
-        name: "error_color",
-        description: "The hex color for error embed color",
-        type: 3
-    },
-    {
-        name: "embed_content",
-        description: "The content for the embed",
-        type: 3
-    },
-    {
-        name: "limit_per_user",
-        description: "The Limit per user for creating tickets",
-        type: 4
-    },
-    {
-        name: "default_name",
-        description: "The default name for ticket name",
-        type: 3
-    },
-    {
-        name: "log_channel",
-        description: "The log channel to send create / delete ticket",
-        type: 7,
-        channel_types: [0]
+        name: "role",
+        description: "Configuration your staff role to view tickets",
+        type: 1,
+        options: [
+            {
+                name: "staff",
+                description: "The staff role to (view ticket / send messages)",
+                type: 8,
+                required: true
+            },
+            {
+                name: "managers",
+                description: "The managers role to (view ticket / send messages / manage channels / manage messages)",
+                type: 8,
+                required: true
+            }
+        ]
     }
 ]
